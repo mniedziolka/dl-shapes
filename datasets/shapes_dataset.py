0926f8f7a19bb589f01895c757175d08ec26953f
niedziol <michalekniedziolka@gmail.com>
2021-04-16 01:27:16 +0200

    Add transformers for data augmentation

---
+++ datasets/shapes_dataset.py
@@ -0,0 +1,62 @@
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# Ignore warnings
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+class ShapesDataset(Dataset):
+
+    def __init__(
+            self,
+            csv_file,
+            root_dir,
+            transform_images=None,
+            transform_all=None
+    ):
+        self.shapes_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform_images = transform_images
+        self.transform_all = transform_all
+
+    def __len__(self):
+        return len(self.shapes_frame)
+
+    def __getitem__(self, idx):
+        raise NotImplementedError('You chose abstract dataset!')
+
+
+class ShapesClassificationDataset(ShapesDataset):
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        shapes = self.shapes_frame.iloc[idx, 1:]
+        shapes = np.array([shapes])
+        shapes = shapes.astype('float').flatten()
+        shapes[shapes > 0] = 1
+
+        img_name = os.path.join(self.root_dir,
+                                self.shapes_frame.iloc[idx, 0])
+
+        image = io.imread(img_name)
+
+        image = transforms.ToPILImage()(image)
+
+        if self.transform_all:
+            image, shapes = self.transform_all((image, shapes))
+
+        if self.transform_images:
+            image = self.transform_images(image)
+
+        return image, shapes
---
+++ datasets/transformers.py
@@ -0,0 +1,49 @@
+from torchvision import transforms
+
+import torch
+import torchvision.transforms.functional as TF
+
+
+class RandomHorizontalFlip:
+
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, sample):
+        image, target = sample
+
+        if torch.rand(1) < self.p:
+            image = transforms.RandomHorizontalFlip(1)(image)
+            target[[2, 4]] = target[[4, 2]]
+
+        return image, target
+
+
+class RandomVerticalFlip:
+
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, sample):
+        image, target = sample
+
+        if torch.rand(1) < self.p:
+            image = transforms.RandomVerticalFlip(1)(image)
+            target[[3, 5]] = target[[5, 3]]
+
+        return image, target
+
+
+class RandomRightRotation:
+
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, sample):
+        image, target = sample
+
+        if torch.rand(1) < self.p:
+            image = TF.rotate(image, -90)
+            target[[3, 4, 5, 2]] = target[[2, 3, 4, 5]]
+
+        return image, target
