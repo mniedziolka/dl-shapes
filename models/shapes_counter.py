f85461f3b9328e2d29ca71f04b6c0d20838d2c50
niedziol <michalekniedziolka@gmail.com>
2021-04-23 04:48:53 +0200

    Add dropout

--- models/shapes_counter.py
+++ models/shapes_counter.py
@@ -16,16 +16,6 @@ class ShapesCounter(nn.Module):
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=4,
-                      out_channels=8,
-                      kernel_size=(3, 3),
-                      padding=(1, 1)),
-            # nn.MaxPool2d(2, 2),
-            nn.BatchNorm2d(8),
-            nn.ReLU()
-        )
-
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=8,
                       out_channels=16,
                       kernel_size=(3, 3),
                       padding=(1, 1)),
@@ -34,7 +24,7 @@ class ShapesCounter(nn.Module):
             nn.ReLU()
         )
 
-        self.conv4 = nn.Sequential(
+        self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels=16,
                       out_channels=32,
                       kernel_size=(4, 4),
@@ -43,7 +33,7 @@ class ShapesCounter(nn.Module):
             nn.ReLU()
         )
 
-        self.conv5 = nn.Sequential(
+        self.conv4 = nn.Sequential(
             nn.Conv2d(in_channels=32,
                       out_channels=64,
                       kernel_size=(5, 5),
@@ -52,44 +42,47 @@ class ShapesCounter(nn.Module):
             nn.ReLU()
         )
 
-        self.conv6 = nn.Sequential(
+        self.conv5 = nn.Sequential(
             nn.Conv2d(in_channels=64,
                       out_channels=128,
                       kernel_size=(6, 6),
                       stride=(2, 2),
                       padding=(1, 1)),
+            nn.Dropout2d(p=0.2),
             nn.BatchNorm2d(128),
             nn.ReLU()
         )
 
-        self.conv7 = nn.Sequential(
+        self.conv6 = nn.Sequential(
             nn.Conv2d(in_channels=128,
                       out_channels=256,
                       kernel_size=(7, 7),
                       padding=(1, 1)),
+            nn.Dropout2d(p=0.2),
             nn.BatchNorm2d(256),
             nn.ReLU()
         )
 
-        self.conv8 = nn.Sequential(
+        self.conv7 = nn.Sequential(
             nn.Conv2d(in_channels=256,
                       out_channels=512,
                       kernel_size=(8, 8)),
+            nn.Dropout2d(p=0.2),
             nn.BatchNorm2d(512),
             nn.ReLU()
         )
 
         self.fc1 = nn.Sequential(
-            nn.Linear(512, 256),
-            nn.BatchNorm1d(256),
-            nn.ReLU()
-        )
-
-        self.fc2 = nn.Sequential(
-            nn.Linear(256, 60),
-            # nn.BatchNorm1d(200),
+            nn.Linear(512, 60),
+            # nn.BatchNorm1d(256),
             # nn.ReLU()
         )
+
+        # self.fc2 = nn.Sequential(
+        #     nn.Linear(256, 60),
+        #     # nn.BatchNorm1d(200),
+        #     # nn.ReLU()
+        # )
         #
         # self.fc3 = nn.Sequential(
         #     nn.Linear(200, 60)
@@ -111,7 +104,7 @@ class ShapesCounter(nn.Module):
         # print(x.shape)
 
         x = self.fc1(x)
-        x = self.fc2(x)
+        # x = self.fc2(x)
         # x = self.fc3(x)
 
         # x = F.relu(F.dropout(self.fc1(x), p=0.6))
